d2e094fc526d25b54942fafa00f62e91e17ee392
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-07-06 11:31:49 -0300

    Exercises 2.2 and 2.3

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Part from './Part';
 
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </>
-);
+const Content = ({ parts }) => {
+  const total = parts.reduce((acc, cur) => acc + cur.exercises, 0);
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+      <p>
+        <strong>total of {total} exercises</strong>
+      </p>
+    </>
+  );
+};
 
 export default Content;
