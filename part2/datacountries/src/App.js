4794802ba82e070adb0bd09bdced6dc6ab3d5b9c
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-07-07 15:40:02 -0300

    Exercise 2.13

--- part2/datacountries/src/App.js
+++ part2/datacountries/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 function App() {
   const [countries, setCountries] = useState([]);
   const [filter, setFilter] = useState('');
+  const [countrySelected, setCountrySelected] = useState('');
 
   useEffect(() => {
     axios
@@ -16,51 +17,73 @@ function App() {
   countries.forEach((country) => {
     const name = country.name.common;
     if (name.toLowerCase().includes(filter.toLowerCase())) {
-      countriesFilter.push(country);
+      countriesFilter.push(name);
     }
   });
 
   const displayCountries = () => {
+    if (countrySelected) {
+      return showCountry(countrySelected);
+    }
     if (countriesFilter.length > 10) {
-      return <p>Too many matches, specify another filter</p>;
+      return <div>Too many matches, specify another filter</div>;
     }
     if (countriesFilter.length > 1) {
       return countriesFilter.map((country) => {
-        const name = country.name.common;
-        return <div key={name}>{name}</div>;
+        return (
+          <div key={country}>
+            {country + ' '}
+            <button
+              onClick={() => {
+                setCountrySelected(country);
+              }}
+            >
+              show
+            </button>
+          </div>
+        );
       });
     }
     if (countriesFilter.length === 1) {
-      const country = countriesFilter[0];
-      return (
-        <div>
-          <h1>{country.name.common}</h1>
-
-          <div>capital {country.capital[0]}</div>
-          <div>area {country.area}</div>
-
-          <h3>languages:</h3>
-          <ul>
-            {Object.values(country.languages).map((lang) => (
-              <li key={lang}>{lang}</li>
-            ))}
-          </ul>
-
-          <img src={country.flags.png} alt={`${country.name.common} flag`} />
-        </div>
-      );
+      return showCountry(countriesFilter[0]);
     }
+    return <div>Not found</div>;
+  };
+
+  const showCountry = (name) => {
+    const filterCountry = countries.filter(
+      (country) => country.name.common === name
+    );
+    const country = filterCountry[0];
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+
+        <div>capital {country.capital[0]}</div>
+        <div>area {country.area}</div>
+
+        <h3>languages:</h3>
+        <ul>
+          {Object.values(country.languages).map((lang) => (
+            <li key={lang}>{lang}</li>
+          ))}
+        </ul>
+
+        <img src={country.flags.png} alt={`${country.name.common} flag`} />
+      </div>
+    );
+  };
+
+  const handleChange = (e) => {
+    setFilter(e.target.value);
+    setCountrySelected('');
   };
 
   return (
     <div className="App">
       <div>
         find countries
-        <input
-          type="text"
-          value={filter}
-          onChange={(e) => setFilter(e.target.value)}
-        />
+        <input type="text" value={filter} onChange={handleChange} />
       </div>
       <div>{displayCountries()}</div>
     </div>
