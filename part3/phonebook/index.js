28579f77e13b11026f74186cba6dda4ebf96fdcd
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-07-12 12:29:56 -0300

    Exercise 3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 let persons = [
   {
@@ -53,6 +54,15 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
+});
+
+app.post('/api/persons', (request, response) => {
+  const id = Math.floor(Math.random() * 1000000);
+  const person = request.body;
+  person.id = id;
+
+  persons = persons.concat(person);
+  response.json(person);
 });
 
 const PORT = 3001;
