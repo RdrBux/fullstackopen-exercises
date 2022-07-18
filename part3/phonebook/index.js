657bf374aadeee7bd951f3da146699949afb26db
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-07-18 13:01:02 -0300

    Exercises 3.13 and 3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+require('dotenv').config();
+const Person = require('./models/person');
 
 const app = express();
 app.use(express.json());
@@ -10,7 +12,7 @@ app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
-let persons = [
+/* let persons = [
   {
     id: 1,
     name: 'Arto Hellas',
@@ -31,38 +33,33 @@ let persons = [
     name: 'Mary Poppendieck',
     number: '39-23-6423122',
   },
-];
-
-const info = `
-  <p>Phonebook has info for ${persons.length} people</p>
-  <p>${new Date()}</p>  
-  `;
+]; */
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then((person) => response.json(person));
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id).then((person) => response.json(person));
 });
 
 app.get('/info', (request, response) => {
-  response.send(info);
+  Person.countDocuments({})
+    .then((docCount) =>
+      response.send(`
+    <p>Phonebook has info for ${docCount} people</p>
+    <p>${new Date()}</p>  
+    `)
+    )
+    .catch((err) => console.log(err));
 });
 
-app.delete('/api/persons/:id', (request, response) => {
+/* app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((person) => person.id !== id);
 
   response.status(204).end();
-});
+}); */
 
 app.post('/api/persons', (request, response) => {
   const person = request.body;
@@ -70,16 +67,17 @@ app.post('/api/persons', (request, response) => {
     return response.status(400).json({ error: 'content missing' });
   }
 
-  if (persons.some((pers) => pers.name === person.name)) {
+  /* if (persons.some((pers) => pers.name === person.name)) {
     return response.status(400).json({ error: 'name must be unique' });
-  }
+  } */
 
-  const id = Math.floor(Math.random() * 1000000);
-  person.id = id;
+  const newPerson = new Person({
+    name: person.name,
+    number: person.number,
+  });
 
-  persons = persons.concat(person);
-  response.json(person);
+  newPerson.save().then((savedPerson) => response.json(savedPerson));
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
