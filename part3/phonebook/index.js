e0015b6c851a6299e571254d009116d2e0fc8ada
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-07-19 11:16:51 -0300

    Exercises 3.15 and 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 const Person = require('./models/person');
 
 const app = express();
-app.use(express.json());
-app.use(express.static('build'));
 
+app.use(express.static('build'));
+app.use(express.json());
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
@@ -39,8 +39,17 @@ app.get('/api/persons', (request, response) => {
   Person.find({}).then((person) => response.json(person));
 });
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => response.json(person));
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((pers) => {
+      console.log(pers);
+      if (pers) {
+        response.json(pers);
+      } else {
+        response.status(404).send({ error: 'unknown endpoint' });
+      }
+    })
+    .catch((err) => next(err));
 });
 
 app.get('/info', (request, response) => {
@@ -51,15 +60,14 @@ app.get('/info', (request, response) => {
     <p>${new Date()}</p>  
     `)
     )
-    .catch((err) => console.log(err));
+    .catch((err) => next(err));
 });
 
-/* app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
-}); */
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => response.status(204).end())
+    .catch((err) => next(err));
+});
 
 app.post('/api/persons', (request, response) => {
   const person = request.body;
@@ -78,6 +86,24 @@ app.post('/api/persons', (request, response) => {
 
   newPerson.save().then((savedPerson) => response.json(savedPerson));
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
