ebd695adf2ca2e63a4243601fb72e1a75fe7e01f
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-07-20 11:51:54 -0300

    Exercise 4.2 - config

--- part4/index.js
+++ part4/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
 const { info, error } = require('./utils/logger');
-require('dotenv').config();
+const config = require('./utils/config');
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -15,7 +15,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
+const mongoUrl = config.MONGODB_URI;
 mongoose.connect(mongoUrl);
 
 app.use(cors());
@@ -35,7 +35,6 @@ app.post('/api/blogs', (request, response) => {
   });
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  info(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  info(`Server running on port ${config.PORT}`);
 });
