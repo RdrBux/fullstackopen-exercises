deb3d9e914fc3330361bdcbdd41a9cc2c6861485
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-07-20 12:10:57 -0300

    Exercise 4.2 - create app.js

--- part4/index.js
+++ part4/index.js
@@ -1,33 +1,11 @@
 /* const http = require('http'); */
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-const { info, error } = require('./utils/logger');
+const http = require('http');
+const app = require('./app');
+const logger = require('./utils/logger');
 const config = require('./utils/config');
-const blogsRouter = require('./controllers/blogs');
 
-const mongoUrl = config.MONGODB_URI;
-mongoose.connect(mongoUrl);
+const server = http.createServer(app);
 
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter);
-/* app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-}); */
-
-/* app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-}); */
-
-app.listen(config.PORT, () => {
-  info(`Server running on port ${config.PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
