730d8ed10a8a2b62ce83d8aab2a02643bc39bb23
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-07-26 10:29:33 -0300

    Exercise 4.18

--- part4/app.js
+++ part4/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('express-async-errors');
 const app = express();
 const cors = require('cors');
+const loginRouter = require('./controllers/login');
 const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
 const middleware = require('./utils/middleware');
@@ -15,6 +16,7 @@ mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/login', loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
