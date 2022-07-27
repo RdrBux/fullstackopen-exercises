e65aea45c06b319a5187985b3f5f8d293fd4aecf
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-07-27 16:34:23 -0300

    Exercise 5.1

---
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const LoginForm = ({
+  handleLogin,
+  username,
+  setUsername,
+  password,
+  setPassword,
+}) => {
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        <label htmlFor="username">username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button>login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
