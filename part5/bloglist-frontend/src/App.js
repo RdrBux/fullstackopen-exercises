4f8ad3b0e0906759ae3570be16f302cf2426a4d3
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-07-29 18:33:52 -0300

    Exercise 5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -120,9 +120,11 @@ const App = () => {
         <BlogForm createBlog={createBlog} />
       </Togglable>
 
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} handleLikes={updateLikes} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} handleLikes={updateLikes} />
+        ))}
     </div>
   );
 };
