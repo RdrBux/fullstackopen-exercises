bf807a6dbab45f11f6eed1469e2aa8cd17a4db70
RdrBux <rodriguezrodrigoemmanuel@gmail.com>
2022-08-01 10:41:26 -0300

    Exercises 5.13 and 5.14

---
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -0,0 +1,54 @@
+import '@testing-library/jest-dom/extend-expect';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Blog from './Blog';
+
+describe('Interactions with blogs', () => {
+  const blog = {
+    user: 'x',
+    likes: 5,
+    author: 'The creator',
+    title: 'Testing this component',
+    url: 'https://www.test.com',
+  };
+
+  test('Displays blog title and author by default, but not url or likes', () => {
+    render(<Blog blog={blog} />);
+    const title = screen.getByText('Testing this component', { exact: false });
+    const author = screen.getByText('The creator', { exact: false });
+    const url = screen.getByText('https://www.test.com', { exact: false });
+    const likes = screen.getByText('likes', { exact: false });
+
+    expect(title).toBeDefined();
+    expect(author).toBeDefined();
+    expect(url).toBeDefined();
+    expect(likes).toBeDefined();
+    expect(title).toBeVisible();
+    expect(author).toBeVisible();
+    expect(url).not.toBeVisible();
+    expect(likes).not.toBeVisible();
+  });
+
+  test('Blog url and likes are shown when clicking the "view" button', async () => {
+    render(<Blog blog={blog} />);
+    const urlStart = screen.getByText('https://www.test.com', { exact: false });
+    const likesStart = screen.getByText('likes', { exact: false });
+
+    expect(urlStart).toBeDefined();
+    expect(likesStart).toBeDefined();
+    expect(urlStart).not.toBeVisible();
+    expect(likesStart).not.toBeVisible();
+
+    const user = userEvent.setup();
+    const button = screen.getByText('view');
+    await user.click(button);
+
+    const urlEnd = screen.getByText('https://www.test.com', { exact: false });
+    const likesEnd = screen.getByText('likes', { exact: false });
+
+    expect(urlEnd).toBeDefined();
+    expect(likesEnd).toBeDefined();
+    expect(urlEnd).toBeVisible();
+    expect(likesEnd).toBeVisible();
+  });
+});
